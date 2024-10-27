e3144ec9830f5ef603d77fe23bdffd4d1b3ada2a
angelafeliciaa <angelafc@students.cs.ubc.ca>
2024-10-27 15:25:52 -0700

    delete rank and select files

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,5 +1,5 @@
 from .github_service import fetch_repo_content
-from .llm_service import find_relevant_files, rank_and_select_files, generate_changes, perform_reflection
+from .llm_service import find_relevant_files, generate_changes, perform_reflection
 from .diff_service import generate_diff, format_diff_indentation
 from .supabase_service import log_generation
 from .visualization import visualize_import_graph
@@ -7,7 +7,6 @@ from .visualization import visualize_import_graph
 __all__ = [
     'fetch_repo_content',
     'find_relevant_files',
-    'rank_and_select_files',
     'generate_changes',
     'perform_reflection',
     'generate_diff',
